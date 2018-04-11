677308fe8874cd6137642fb3a3438df9e75eb595
adonskoy <x664@yandex.ru>
2018-04-11 09:03:43 +0300

    code style changes

--- 2sem/txt2bin.cpp
+++ 2sem/txt2bin.cpp
@@ -2,41 +2,38 @@
 // Created by Антон Донской on 31.03.2018.
 //
 
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-int *read_mass(const char* file_name, int *size){
-    ifstream instr (file_name,ios::in);
-    int  capacity=2;
+int *read_mass(const char* file_name, int *size) {
+    ifstream instr(file_name, ios::in);
+    int capacity = 2;
     auto *mass = new int[capacity];
-    while(instr){
+    while (instr) {
         instr >> mass[*size];
-        *size = (*size)+1;
-        if(*size == capacity){
-            auto *new_mass = new int[capacity*2];
-            for(int i(0); i<capacity; ++i){
-                new_mass[i]=mass[i];
+        *size = (*size) + 1;
+        if (*size == capacity) {
+            auto *new_mass = new int[capacity * 2];
+            for (int i(0); i < capacity; ++i) {
+                new_mass[i] = mass[i];
             }
-            capacity*=2;
+            capacity *= 2;
             delete[] mass;
-            mass=new_mass;
+            mass = new_mass;
         }
     }
     return mass;
 }
 
-
-
-void write_mass_binary(const char* file_name,int* mass,int count){
-    ofstream outstr (file_name,ios::out | ios::binary);
-
-    outstr.write((char*)mass,sizeof(int)*count);
+void write_mass_binary(const char* file_name,int* mass,int count) {
+    ofstream outstr(file_name, ios::out | ios::binary);
+    outstr.write((char *) mass, sizeof(int) * count);
 }
 
-int main(){
+int main() {
     int size = 0;
-    int * mass = read_mass("input.txt", &size);
+    int *mass = read_mass("input.txt", &size);
     write_mass_binary("output.bin", mass, size);
     return 0;
 }
